302e0e312fa2fef54994c562082ba9a33b3c9c3c
Steve Sanbeg <steve.sanbeg@gmail.com>
2025-11-27 12:53:26 -0500

    fix generic emitter

--- demo_stripe.py
+++ demo_stripe.py
@@ -5,13 +5,28 @@ from ili9341_generic import GenericParallelEmitter
 from machine import Pin
 from time import sleep_ms, ticks_ms
 
+# control pins
 LCD_RD = Pin.board.A0 #a0
 LCD_WR = Pin.board.A1
 LCD_RS = Pin.board.A2
 LCD_CS = Pin.board.A3
 LCD_RST = Pin.board.A4
 
-lcd = Display(rst = LCD_RST, cs=LCD_CS, rs = LCD_RS, wr = LCD_WR, rd = LCD_RD, emitter = NucleoEmitter())
+#data pins
+#data pins
+LCD_D0 = Pin.board.D8 #a9
+LCD_D1 = Pin.board.D9 #c7
+LCD_D2 = Pin.board.D2 #a10
+LCD_D3 = Pin.board.D3 #b3
+LCD_D4 = Pin.board.D4 #b5
+LCD_D5 = Pin.board.D5 #b4
+LCD_D6 = Pin.board.D6 #b10
+LCD_D7 = Pin.board.D7 #a8
+
+fast_emitter = NucleoEmitter()
+slow_emitter = GenericParallelEmitter(LCD_D0, LCD_D1, LCD_D2, LCD_D3, LCD_D4, LCD_D5, LCD_D6, LCD_D7) 
+
+#lcd = Display(rst = LCD_RST, cs=LCD_CS, rs = LCD_RS, wr = LCD_WR, rd = LCD_RD, emitter = fast_emitter)
 
 def demo():
     start = ticks_ms()
@@ -25,7 +40,15 @@ def demo():
     lcd.draw_rect(280,0,40,240, Colors.BLACK)# BLACK
     
     print(f"Elapsed time ms={ticks_ms()-start}")
+    
 
-def restart(speed = True):   
+
+def restart(fast = True):
+    global lcd
+    if fast:
+        lcd = Display(rst = LCD_RST, cs=LCD_CS, rs = LCD_RS, wr = LCD_WR, rd = LCD_RD, emitter = fast_emitter)
+    else:
+        lcd = Display(rst = LCD_RST, cs=LCD_CS, rs = LCD_RS, wr = LCD_WR, rd = LCD_RD, emitter = slow_emitter)
+
     lcd.init()
     demo()
--- ili9341_generic.py
+++ ili9341_generic.py
@@ -1,19 +1,28 @@
 class GenericParallelEmitter:
     "Emitter which uses generic Pin functions to emit parallel data"
     def __init__(self, d0, d1, d2, d3, d4, d5, d6, d7):
-        self.pins = [d0, d1, d2, d3, d4, d5, d6, d7]
+        self._pins = [d0, d1, d2, d3, d4, d5, d6, d7]
         
     def init(self):
-        for pin in self.pins:
+        for pin in self._pins:
             pin.init(pin.OUT)
 
     @staticmethod
+    @micropython.viper
     def _emit_bit(data, pin, bit):
-        boolean = (data & (1<<bit)) > 0
+        boolean = (int(data) & (1<<int(bit))) > 0
         pin.value(boolean)
     
-    def emit(data):
-        for bit, pin in self.pins:
-            _emit_bit(data, pin, bit)
-            
+    def emit(self, data):
+        emit_bit = self._emit_bit
+        pins = self._pins
+        d = int(data)
+        emit_bit(d, pins[0], 0)
+        emit_bit(d, pins[1], 1)
+        emit_bit(d, pins[2], 2)
+        emit_bit(d, pins[3], 3)
+        emit_bit(d, pins[4], 4)
+        emit_bit(d, pins[5], 5)
+        emit_bit(d, pins[6], 6)
+        emit_bit(d, pins[7], 7)
     
